a7d82b8059a72b9e023064ea666ffeeb8246ecd0
edugaRP <edulindorodrigues@gmail.com>
2023-09-06 14:33:51 -0300

    initial config

---
+++ src/main/java/com/PicPaySimplified/PicPaySimplified/PicPaySimplifiedApplication.java
@@ -0,0 +1,13 @@
+package com.PicPaySimplified.PicPaySimplified;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPaySimplifiedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPaySimplifiedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/PicPaySimplified/PicPaySimplified/PicPaySimplifiedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.PicPaySimplified.PicPaySimplified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPaySimplifiedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
