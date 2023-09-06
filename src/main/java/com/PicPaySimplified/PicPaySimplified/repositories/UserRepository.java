ec750a16bd887225c7fdfcb51a57a0e2908ed229
edugaRP <edulindorodrigues@gmail.com>
2023-09-06 14:34:49 -0300

    Creating User Repository

---
+++ src/main/java/com/PicPaySimplified/PicPaySimplified/repositories/UserRepository.java
@@ -0,0 +1,16 @@
+package com.PicPaySimplified.PicPaySimplified.repositories;
+
+import com.PicPaySimplified.PicPaySimplified.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+
+    Optional<User> findUserByCpf(String cpf);
+
+    Optional<User> findUserByID(Long id);
+
+}
